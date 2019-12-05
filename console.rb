fc28e35d8521fabbc88c163c9a7067e1a71d4dc2
Matteo Fusillo <matt.fusillo@gmail.com>
2019-12-05 15:27:30 +0000

    CRUD and test for star

--- console.rb
+++ console.rb
@@ -4,9 +4,15 @@ require_relative('models/casting')
 require_relative('models/star')
 
 Movie.delete_all()
+Star.delete_all()
 
 movie1 = Movie.new({ 'title' => 'Die Hard', 'genre' => 'action'})
 movie1.save()
+
+
+
+star1 = Star.new({ 'first_name' => 'Bruce', 'last_name' => 'Willis'})
+star1.save()
 
 binding.pry
 nil
--- models/star.rb
+++ models/star.rb
@@ -1,3 +1,50 @@
+require_relative('../db/sql_runner')
+require_relative('movie')
+
 class Star
+
+  attr_reader :id
+  attr_accessor :first_name, :last_name
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @first_name = options['first_name']
+    @last_name = options['last_name']
+  end
+
+  def save()
+    sql = "INSERT INTO stars (
+    first_name,
+    last_name
+    ) VALUES (
+    $1,
+    $2
+    ) RETURNING id;"
+    values = [@first_name, @last_name]
+    @id = SqlRunner.run(sql, values)[0]['id'].to_i
+  end
+
+  def self.all()
+    sql = "SELECT * FROM stars;"
+    stars = SqlRunner.run(sql)
+    return map_stars(stars)
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM stars;"
+    SqlRunner.run(sql)
+  end
+
+  def update()
+    sql = "UPDATE stars SET first_name = $1, last_name = $2 WHERE id = $3;"
+    values = [@first_name, @last_name, @id]
+    SqlRunner.run(sql, values)
+  end
+
+  def self.map_stars(star_data)
+    return star_data.map {|star| Star.new(star)}
+  end
+
+
 
 end
